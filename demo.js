5dcb4145acd7190590d2271c33454b11d2c067f3
rishiraj007 <rishiraj@mounttalent.com>
2023-01-22 20:46:14 +0530

    [Prabhu] Add: Demo Program

---
+++ demo.js
@@ -0,0 +1,16 @@
+let a=10;
+let b=20;
+let c=a+b;
+console.log("Total value:- "+c);
+
+let s1="Prabhu";
+let s2="Goyal";
+let s=s1+s2;
+console.log("String concat:"+s);
+
+let d=new Date();
+let today="month is:- "+(d.getMonth()+1)+" date:- "+d.getDate()+" year:- "+d.getFullYear();
+let time=d.getTime();
+
+console.log(today);
+console.log(time);
